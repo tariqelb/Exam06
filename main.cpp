a69f6a84045908a84e3316e15385f68c2bbbba08
Tariq El bouhali <tariqelbouhali039@gmqil.com>
2023-07-31 20:14:44 +0100

    v2

--- main.cpp
+++ main.cpp
@@ -9,12 +9,13 @@
 #include <stdlib.h>
 #include <strings.h>
 #include <iostream>
+#include <netinet/in.h>
 
 struct	client
 {
-	int			fd;
+	int					fd;
 	struct sockaddr_in	addr;	
-}
+};
 
 struct	server
 {
@@ -26,7 +27,7 @@ struct	server
 	fd_set			tmp_write;
 	int			fd_max;
 	int			nbr_of_clt;
-}
+};
 
 
 void	ft_max_fd(struct server *srv, struct client *clt)
@@ -35,7 +36,7 @@ void	ft_max_fd(struct server *srv, struct client *clt)
 
 	i = 0;
 	srv->fd_max = srv->fd;
-	while (clt[i] != NULL)
+	while (i < srv->nbr_of_clt)
 	{
 		if (srv->fd_max < clt->fd)
 			srv->fd_max = clt->fd;
@@ -43,16 +44,17 @@ void	ft_max_fd(struct server *srv, struct client *clt)
 	}
 }
 
-strcut client	ft_new_client(int *nbr_of_clt, struct client *clt)
+struct client	*ft_new_client(int *nbr_of_clt, struct client *clt)
 {
 	struct client *new_clt = NULL;
 
 	if (*nbr_of_clt == 0)
 	{
-		new_clt = (struct client *) malloc(sizeof(struct client) * 2);
+	/*	new_clt = (struct client *) malloc(sizeof(struct client) * 2);
 		if (new_clt == NULL)
 		{
-			//free and exit();
+			write(2, "Fatal error\n", 12);
+			exit(1);
 		}
 		new_clt[0].fd = -1;
 		new_clt[1] = NULL;
@@ -60,44 +62,53 @@ strcut client	ft_new_client(int *nbr_of_clt, struct client *clt)
 	}
 	else
 	{
-		struct client *temp = NULL;
-		temp = (struct client *) malloc(sizeof(strcut client) * (*nbr_of_clt + 1));
-		if (temp == NULL)
+		new_clt = (struct client *) malloc(sizeof(strcut client) * (*nbr_of_clt + 2));
+		if (new_clt == NULL)
 		{
-			//free and exit;
+			free(clt);
+			write(2, "Fatal error\n", 12);
+			exit(1);
 		}
 		int i = 0;
 		while (i < *nbr_of_clt)
 		{
-			temp[i].fd =  clt[i].fd;
-			temp[i].clt = clt[i].addr;
+			new_clt[i].fd =  clt[i].fd;
+			new_clt[i].clt = clt[i].addr;
 			i++;
 		}
-
+		new_clt[i].fd = -1;
+		new_clt[i + 1] = NULL;
+		free(clt);
+		clt = NULL;*/
 	}
+		return (new_clt);
 }
+//newClient.fd = accept(web.servers[i].socketFd[j], (struct sockaddr *)&newClient.addr, &newClient.len);
 
-struct client	ft_handle_connection(struct server *srv, struct client *clt)
+struct client	*ft_handle_connection(struct server *srv, struct client *clt)
 {
 	int 	i;
 	int	clt_index;
 
 	i = 0;
-	if (FD_ISSET(srv->fd, srv->tmp_read))
+	if (FD_ISSET(srv->fd, &srv->tmp_read))
 	{
 		clt = ft_new_client(&srv->nbr_of_clt, clt);
+		clt[srv->nbr_of_clt].fd = accept(srv->fd, (struct sockaddr *)&clt[srv->nbr_of_clt].addr, sizeof(clt[srv->nbr_of_clt].addr));
+		std::cout << "A new client seccefully created" << std::endl;
+		free(clt);
+		exit(1);
 	}
-	
-	while (i < )
+	return (clt);	
 }
 
 int	main(int ac, char **av)
 {
-	int			fd;
-	int			port;
-	int			status;
-	int			backlog;
-	client			clt;
+	int				fd;
+	int				port;
+	int				status;
+	int				backlog;
+	client			*clt;
 	server			srv;	
 
 	srv.fd = -1;
@@ -122,8 +133,8 @@ int	main(int ac, char **av)
 	}
 	srv.addr.sin_family = AF_INET;
 	srv.addr.sin_port = htons(port);
-	srv.inet_pton(AF_INET, "127.0.0.1", &srvaddr.sin_addr);
-	status = bind(srv.fd, srv.addr, sizeof(addr));
+	srv.addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	status = bind(srv.fd, srv.addr, sizeof(srv.addr));
 	if (status == -1)
 	{
 		write(2, "Fatal error\n", 12);
@@ -139,22 +150,22 @@ int	main(int ac, char **av)
 	}
 	FD_ZERO(&srv.read);
 	FD_ZERO(&srv.write);
-	FD_SET(srv.fd, srv.read);
-	FD_SET(srv.fd, srv.write);
+	FD_SET(srv.fd, &srv.read);
+	FD_SET(srv.fd, &srv.write);
 	while (1)
 	{
 		FD_ZERO(&srv.tmp_read);
 		FD_ZERO(&srv.tmp_write);
 		srv.tmp_read = srv.read;
 		srv.tmp_write = srv.write;	
-		status = select(isrv.fd_max, srv.tmp_read, srv.tmp_write, 0 , 0);
+		status = select(srv.fd_max, &srv.tmp_read, &srv.tmp_write, 0 , 0);
 		if (status <= 0)
 		{
 			write(2, "Fatal error\n", 12);
 			exit(1);
 		}
 		else
-			clt = ft_handle_connection(&sev, clt);	
+			clt = ft_handle_connection(&srv, clt);	
 	}
 	
 	
